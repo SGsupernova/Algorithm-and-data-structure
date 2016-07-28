3b2474c7f9a2eed5e348e211e07a938e62a9f347
Tae Seung <taeseung@Taeui-MacBook-Pro.local>
2016-07-28 23:24:16 +0900

    delete!

--- algorithm_homework/quicksort/medianof3.c
+++
@@ -1,7 +0,0 @@
-void * medianof3(void *lo, void *mid, void *hi, _Cmpfun *cmp) {
-	return cmp(lo, mid) < 0 ?
-		cmp(mid, hi) < 0 ? mid :
-		cmp(lo, hi) < 0 ? hi : lo :
-		cmp(lo, hi) < 0 ? lo :
-		cmp(mid, hi) < 0 ? hi : mid;
-}
